bcfb26bf2cfc3164085d5c138ec563b3bb60e4f2
joepeth matining <matining.joepeth30@gmail.com>
2024-05-23 17:00:08 +0800

    take-home exam

---
+++ index.js
@@ -0,0 +1,42 @@
+import express from "express";
+import { dirname } from "path";
+import { fileURLToPath } from "url";
+import bodyParser from "body-parser";
+const __dirname = dirname(fileURLToPath(import.meta.url));
+
+const app = express();
+const port = 3000;
+
+var userIsAuthorised = false;
+
+app.use(bodyParser.urlencoded({ extended: true }));
+
+
+function userCheck(req, res, next) {
+  const username = req.body["username"]
+    const password = req.body["password"];
+    if (username === "admin" && password === "thereisnopassword") {
+      userIsAuthorised = true;
+    }
+    next();
+  }
+ app.use(userCheck);
+
+ app.get("/", (req, res) => {
+  res.sendFile(__dirname + "/public/index.html");
+ 
+  });
+
+
+  app.post("/submit", (req, res) => {
+    if (userIsAuthorised) {
+      res.sendFile(__dirname + "/public/item.html");
+    } else {
+      res.sendFile(__dirname + "/public/index.html");
+    }
+  });
+
+app.listen(port, () => {
+    console.log(`Listening on port ${port}`);
+  });
+  
